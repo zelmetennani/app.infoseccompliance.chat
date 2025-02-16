816ca028fae77ab5f08d03949d68fff7719d5d10
Zack <zacharyelmetennani@gmail.com>
2025-02-16 11:21:14 +0700

    re-org

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "your-api-key",
+    authDomain: "your-auth-domain.firebaseapp.com",
+    projectId: "your-project-id",
+    storageBucket: "your-storage-bucket.appspot.com",
+    messagingSenderId: "your-messaging-sender-id",
+    appId: "your-app-id"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig); 
